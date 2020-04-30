d23b761063bbe28dcf4b533e7279b027f6cb5bcb
impactlearner <time.darden@gmail.com>
2020-04-30 20:31:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_concat (array1, array2)
 array1.concat(array2)
 end
 
-def using_insert(array, element.to_i)
+def using_insert(array, element)
+  element.to_i
   array.insert(element[3])
 end
